a8f265b2d739ffcafcfc459a15bb03d5bcad8c12
Benjamin Petetot <bpetetot@gmail.com>
2020-08-17 15:19:17 +0200

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,12 @@
 
 echo "Hello $GITHUB_REPOSITORY"
 
+npx create-vuepress website
+
 git clone "https://github.com/$GITHUB_REPOSITORY.wiki.git"
+
+echo "---------"
+ls
+echo "---------"
+ls website
+echo "---------"
